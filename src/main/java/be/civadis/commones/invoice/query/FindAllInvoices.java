565e16d26168561bcb871b07244ef77cd67c3e56
philippe <phw@civadis.be>
2019-07-28 15:44:30 +0200

    FindAllInvoices

---
+++ src/main/java/be/civadis/commones/invoice/query/FindAllInvoices.java
@@ -0,0 +1,16 @@
+package be.civadis.commones.invoice.query;
+
+import be.civadis.tools.es.BaseQuery;
+
+/**
+ * FindAllInvoices
+ */
+public class FindAllInvoices extends BaseQuery{
+
+    public FindAllInvoices(String tenantId) {
+        super(tenantId);
+        // TODO Auto-generated constructor stub
+    }
+
+    
+}
